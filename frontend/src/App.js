883f9f36734abc19f9d81107820d141c09699a2c
Ethan Wright <wrightet707@gmail.com>
2021-04-18 20:58:43 -0700

    footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,16 +7,24 @@ function App() {
 
   useEffect(() => {
     axios.get("https://api.thedogapi.com/v1/images/search").then(res => {
-      setImgSrc(res.data);
+      setImgSrc(res.data[0].url);
     });
   }, []);
 
 
   return (
     <div className="App">
-        <h1>test</h1>
-      
-      {imgSrc !== null ? <img src={imgSrc} /> : null}
+      <header></header>
+        <h1>Get a Dog!</h1>
+      {console.log(imgSrc)}
+      {imgSrc !== null ? <img  className='landing-image' src={imgSrc} alt=''/> : null}
+      <footer>
+        <ul className='footer-links'>
+          <li className='link'><a target="_blank" rel="noreferrer" href='https://www.linkedin.com/in/ethan-wright-91a75270/'>LinkedIn</a></li>
+          <li className='link'><a target="_blank" rel="noreferrer" href='https://github.com/wrightet/get_a_dog'>GitHub</a></li>
+          <li className='link'><a target="_blank" rel="noreferrer" href='https://wrightet.github.io/ethan-wright/'>Portfolio</a></li>
+        </ul>
+      </footer>
     </div>
   );
 }
