a37a316cdae8e7786f1e25f205f22d4a35e8a49d
Ethan Wright <wrightet707@gmail.com>
2021-04-20 18:44:28 -0700

    clear loading errors with weight and height

--- frontend/src/components/dog_show.js
+++ frontend/src/components/dog_show.js
@@ -1,10 +1,34 @@
 import React,  {useState, useEffect} from 'react';
+import {gql, useQuery} from '@apollo/client';
 import axios from 'axios';
 import '../App.css';
+// const GET_DOG = gql`
+//     query getDog{
+//         id: ID!
+//         name: String!
+//         life_span: String!
+//         origin: String!
+//         weight: String!
+//         height: String!
+//     }
+// `
 function DogShow() {
+    // const { id, name, life_span, origin, weight, height} = useQuery(getDog);
     const [dog, setDog] = useState({});
     useEffect(() => {
-        axios.get('https://api.thedogapi.com/v1/breeds/1').then(res => setDog(res.data))
+        axios.get('https://api.thedogapi.com/v1/breeds/1')
+        .then(
+            res => setDog({
+                name:res.data.name,
+                life_span:res.data.life_span,
+                origin:res.data.origin,
+                weight:res.data.weight.imperial,
+                height:res.data.height.imerial,
+                bred_for: res.data.bred_for,
+                breed_group: res.data.breed_group
+
+            }
+                ))
     }, []);
     console.log('dog',dog)
     return (
@@ -16,10 +40,9 @@ function DogShow() {
                 <li className='dog_fact_el'>Breed Group: {dog.breed_group}</li>
                 <li className='dog_fact_el'>Place of Origin: {dog.origin}</li>
                 <li className='dog_fact_el'>Life Span: {dog.life_span}</li>
-                {/* <li className='dog_fact_el'>Imperial: {dog.height.imperial} inches</li>
-                <li className='dog_fact_el'>Metric: {dog.height.metric} cm</li>
-                <li className='dog_fact_el'>Imperial: {dog.weight.imperial} lbs</li>
-                <li className='dog_fact_el'>Metric: {dog.weight.metric} kg</li> */}
+                 <li className='dog_fact_el'>Height: {dog.height} inches</li>
+                <li className='dog_fact_el'>Weight: {dog.weight} lbs</li>
+                
                 
         </ul>
     </div>
